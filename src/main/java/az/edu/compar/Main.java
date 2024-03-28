b67388ce1bd5dabed4d54e63e5f7ab9965e6d8c4
sananabbasov <sanan_abbasov@list.ru>
2024-03-28 20:23:23 +0400

    while loop

--- src/main/java/az/edu/compar/Main.java
+++ src/main/java/az/edu/compar/Main.java
@@ -2,6 +2,8 @@ package az.edu.compar;
 
 import az.edu.compar.models.Car;
 
+import java.util.Scanner;
+
 // Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -10,29 +12,54 @@ public class Main {
         // Gas station =>
         // Report =>
 
+        Scanner scanner = new Scanner(System.in);
         Car car = new Car();
-
-
+        car.setId(1);
+        car.setModel("Patrol");
+        car.setBrand("Nissan");
+        car.setMillage(120);
+        car.setFuel(100);
+        car.setMaxFuel(100);
+        car.setFuelPerKm(15);
 
         System.out.println("1. Start");
         System.out.println("2. Gas station");
         System.out.println("3. Report");
+        System.out.println("4. Exit");
 
+        int userInput = scanner.nextInt();
 
-        switch (1){
-            case 1:
-                // Start
-                break;
-            case 2:
-                // Gas station
-                break;
-            case 3:
-                // Report
-                break;
-            default:
-                //
-                break;
+        while (userInput != 4){
+
+            System.out.println("1. Start");
+            System.out.println("2. Gas station");
+            System.out.println("3. Report");
+            System.out.println("4. Exit");
+
+            userInput = scanner.nextInt();
+
+            switch (userInput){
+                case 1:
+                    // Start
+                    System.out.println("Herekete basladiq.");
+                    break;
+                case 2:
+                    // Gas station
+                    System.out.println("Benzin doldurulur.");
+                    break;
+                case 3:
+                    // Report
+                    System.out.println("Statistika.");
+                    break;
+                default:
+                    //
+                    break;
+            }
         }
+
+
+
+
 
 
     }
